ddf72a0cf97418c1d02f99fdd3d15e4d32324013
Bryan Conrad <bkconrad@gmail.com>
2016-05-05 04:54:25 -0700

    try to bootstrap the elasticsearch node within the instance

--- cloudformation/deploy.sh
+++ cloudformation/deploy.sh
@@ -1,6 +1,38 @@
 #!/bin/bash
 set -x
 
+# try curling the endpoint, if it fails we assume that the access policy needs updating
+if curl http://search-docker-elk-l36mttokyybzicxf5p23dxb234.us-east-1.es.amazonaws.com | grep 'not authorized' ; then
+
+read -r -d '' POLICY <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "*"
+      },
+      "Action": "es:*",
+      "Condition": {
+        "IpAddress": {
+          "aws:SourceIp": [
+            "$(curl http://169.254.169.254/latest/meta-data/public-ipv4)"
+          ]
+        }
+      },
+      "Resource": "$ELASTICSEARCH_ARN/*"
+    }
+  ]
+}
+EOF
+
+  aws --region "$REGION" es update-elasticsearch-domain-config \
+    --domain-name "$ELASTICSEARCH_DOMAIN_NAME" \
+    --access-policies "$POLICY"
+fi
+
 # sync logstash config bucket
 aws --region "$REGION" s3 sync "s3://$S3_BUCKET/" "/opt/docker-elk/logstash/config/"
 
