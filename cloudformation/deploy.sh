5458ef6c0d24b50aca6d4bd77b5b0245d1216370
Bryan Conrad <bkconrad@gmail.com>
2016-05-05 00:14:12 -0700

    first pass cloudformation setup

---
+++ cloudformation/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -x
+
+# sync logstash config bucket
+aws --region "$REGION" s3 sync "s3://$S3_BUCKET/" "/opt/docker-elk/logstash/config/"
+
+# bounce docker containers
+docker-compose -f docker-compose-production.yml pull
+docker-compose -f docker-compose-production.yml down
+docker-compose -f docker-compose-production.yml up -d
